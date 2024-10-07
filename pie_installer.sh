b5f4511052228c433c243ba5fe46e72d40b53f34
abdurahman-harouat <abderrahmane.harouat@gmail.com>
2024-10-07 10:12:22 +0100

    adding pie installer

---
+++ pie_installer.sh
@@ -0,0 +1,19 @@
+# todo : check dependencies , and if they do not exist , install them
+
+echo "Downloading repository ...."
+git clone https://github.com/abdurahman-harouat/pie
+cd pie
+
+echo "building ...."
+go build -o pie
+
+
+echo "installing ...."
+# delete older pie version if exists
+sudo rm /usr/local/bin/pie
+
+# copy pie to bin folder
+sudo mv pie /usr/local/bin/
+
+# allow pie file to run as a program
+sudo chmod +x /usr/local/bin/pie
